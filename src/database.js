fdf59ad8371086e853034d36537f7e1c1dbece6e
Oky Melina <okymelina98@gmail.com>
2021-08-01 22:14:42 -0600

    Usefull server

---
+++ src/database.js
@@ -0,0 +1,14 @@
+import monngoose from "mongoose";
+
+export async function connect() {
+    try {
+        monngoose.connect('mongodb://localhost:27017/prueba', {
+            useNewUrlParser: true
+        })
+        console.log('db is connected')
+    }catch(e){
+        console.log('Something went wrong');
+        console.log(e);
+    }
+
+}

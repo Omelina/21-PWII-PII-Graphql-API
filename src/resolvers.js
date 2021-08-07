16e59aa6e4f7d27ea008a4d5c5ceee4777352717
Oky Melina <okymelina98@gmail.com>
2021-08-07 15:59:53 -0600

    searching by description

--- src/resolvers.js
+++ src/resolvers.js
@@ -34,8 +34,9 @@ export const resolvers = {
             const message = data.data
             for (let i = 0; i < news.length; i++) {
                 if (news[i].user_id == data.user) {
-                    let posicion = news[i].title.toLowerCase().indexOf(message.toLowerCase());
-                    if (posicion !== -1) {
+                    let t = news[i].title.toLowerCase().indexOf(message.toLowerCase());
+                    let d = news[i].description.toLowerCase().indexOf(message.toLowerCase());
+                    if (t !== -1 || d !== -1) {
                         a.push(news[i]);
                     }
                 }
